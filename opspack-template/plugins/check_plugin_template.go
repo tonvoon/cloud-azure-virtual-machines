4b7e5adc317b8b640de5686404d8140b2b13f8fc
Genaro Bedenko <genarobedenko@hotmail.com>
2018-01-08 15:01:18 +0000

    Modified example plugin

--- opspack-template/plugins/check_plugin_template.go
+++ opspack-template/plugins/check_plugin_template.go
@@ -16,16 +16,4 @@ func main() {
 	// Main function creates the new plugin instance, then calls function to retrieve metrics
 	// For the required metric it retrieves the stat and adds it to the output
 	// Switches into various cases depending on which metric is needed
-
-	check := plugin.New("check_example", "v1.0.0") 
-	if err := check.ParseArgs(&opts); err != nil {
-		// Handles error if incorrect arguments entered
-		check.ExitUnknown("Error parsing arguments: %s", err)
-	}
-
-	defer check.Final() // Calculates the final output after function completes
-
-	check.AllMetricsInOutput = true // So that all metrics are added to the summary message
-
-	check.AddMetric(opts.Mode, 10, "%", opts.Warning, opts.Critical)
 }
